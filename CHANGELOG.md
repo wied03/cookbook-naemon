0124e0450a53ba9976776692725ea76628dc1c8f
Brady Wied <brady@wied.us>
2014-05-18 16:44:38 -0500

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of ../standalone_cookbooks/naemon
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             '../standalone_cookbooks/naemon'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ../standalone_cookbooks/naemon'
+long_description 'Installs/Configures ../standalone_cookbooks/naemon'
+version          '0.1.0'
+
