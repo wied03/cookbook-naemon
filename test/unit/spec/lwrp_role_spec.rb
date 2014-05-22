48e2806591645d79ae43a7c7f687b781a0e25c5b
Brady Wied <brady@wied.us>
2014-05-21 19:27:00 -0500

    Make sure the multiple role stuff works

--- test/unit/spec/lwrp_role_spec.rb
+++ test/unit/spec/lwrp_role_spec.rb
@@ -57,16 +57,54 @@ describe 'naemon::lwrp:role' do
     pending 'Write this test'
   end
 
-  it 'works properly with 1 host in 2 different roles with 2 services' do
+  it 'works properly with 2 hosts in 2 different roles with 2 services' do
     # arrange
+    stub_search(:node, 'role:db')
+    .and_return([{
+                     fqdn: 'host1.stuff.com',
+                     ipaddress: '172.16.0.1'
+                 }])
+    stub_search(:node, 'role:web')
+    .and_return([{
+                     fqdn: 'host2.stuff.com',
+                     ipaddress: '172.16.0.2'
+                 }])
+    temp_lwrp_recipe contents: <<-EOF
+            naemon_role 'chef role blah' do
+              roles 'db'
+              service 'naemon svc desc' do
+                check_command 'the_command2'
+              end
+            end
 
-    # act
+            naemon_role 'the web role' do
+              roles 'web'
+              service 'apache' do
+                check_command 'apache_command'
+              end
+            end
+    EOF
 
-    # assert
-    pending 'Write this test'
+    # act + assert
+    svc_resource_1 = @chef_run.find_resource('naemon_service', 'naemon svc desc')
+    expect(svc_resource_1).to_not be_nil
+    expect(svc_resource_1.check_command).to eq('the_command2')
+    expect(svc_resource_1.host).to eq('host1.stuff.com')
+    host_resource_1 = @chef_run.find_resource('naemon_host', 'host1.stuff.com')
+    expect(host_resource_1).to_not be_nil
+    host_resource_1.hostname.should == 'host1.stuff.com'
+    host_resource_1.address.should == '172.16.0.1'
+    host_resource_2 = @chef_run.find_resource('naemon_host', 'host2.stuff.com')
+    expect(host_resource_2).to_not be_nil
+    host_resource_2.hostname.should == 'host2.stuff.com'
+    host_resource_2.address.should == '172.16.0.2'
+    svc_resource_2 = @chef_run.find_resource('naemon_service', 'apache')
+    expect(svc_resource_2).to_not be_nil
+    expect(svc_resource_2.check_command).to eq('apache_command')
+    expect(svc_resource_2.host).to eq('host2.stuff.com')
   end
 
-  it 'works properly with 2 hosts in 2 different roles with 2 services' do
+  it 'works properly with 1 hosts in 2 different roles with 2 services' do
     # arrange
 
     # act
