7c7d09d710eed1622486abbefd05eac085cd6796
Brady Wied <brady@wied.us>
2014-05-21 23:24:43 -0500

    Put tested code in

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,21 @@
 #
 # Copyright 2014, BSW Technology Consulting
 #
+
+# Sample usage (tested on real Chef)
+
+# naemon_command 'the_command' do
+#   command_line '/etc/do_stuff'
+# end
+#
+# naemon_role 'db' do
+#   service 'naemon apache service' do
+#     check_command 'the_command'
+#   end
+# end
+#
+# This would actually need to be the Naemon service
+# service 'apache2' do
+#   action :nothing
+#   subscribes :restart, 'template[/etc/naemon/conf.d/hosts.cfg]', :delayed
+# end
